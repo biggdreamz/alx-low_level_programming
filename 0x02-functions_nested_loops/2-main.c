beac97e9e13a1e7502be7634a38410bba4655030
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-06-25 23:11:58 +0100

    main updated

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,24 +1,12 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
+#include "main.h"
 
-#define PI 3.142
-
-void sayHello(){
-   printf("Hello World\n");
-}
-
-int add(int a, int b){
-   int result;
-   result = a+b;
-   return result;
-}
-
-double area(double radius){
-   double areaofcircle = PI*pow(radius, 2);
-   return areaofcircle;
-}
-
-int length(char *x){
-   return strlen(x);
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet_x10();
+    return (0);
 }
