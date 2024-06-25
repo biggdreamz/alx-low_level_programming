793f1ec453893eb24ace7fabfa43ff627542aaa7
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-06-25 21:58:27 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints a random number and states whether
- *        it is positive, negative, or zero.
- *
- * Return: Always 0.
+ * main - Print _putchar
+ * Return: Always 0
  */
- 
-int main(void) 
-
+int main(void)
 {
-    printf("_putchar\n");
-	
-    return 0;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
