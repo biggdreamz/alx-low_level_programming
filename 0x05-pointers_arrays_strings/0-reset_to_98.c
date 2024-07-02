a4114a9e4813282b45c069fda05bf2b7c8671baf
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-07-02 09:05:07 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,11 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
- /**funtion for reset_to_98
+ /**
+ *funtion for reset_to_98
+ * parameter for n
  */
 void reset_to_98(int *n)
 {
-/** parameter for n
-*/
 	*n = 98;
 }
