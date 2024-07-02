2aac15c51f1443084ef58b24cefcf424d2617068
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-07-02 11:04:13 +0100

    heading forward

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string followed by a new line.
+ *
+ * @str: Pointer to the string to be printed.
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+        putchar(*str);
+		str++;
+		}
+	putchar('\n');
+}
