fe908d9eb681bd7bfae5512693d04a8f99700e36
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-07-02 21:59:38 +0100

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,39 +1,40 @@
 #include "main.h"
-#include <stdio.h>
-#include <limits.h>
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: Pointer to the string to be converted.
+ * _atoi - Convert a string to an integer.
+ * @s: The pointer to convert
  *
- * Return: The integer value of the string.
+ * Return: A integer
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int result = 0;
+	int c = 0;
+	unsigned int ni = 0;
+	int min = 1;
+	int isi = 0;
 
-	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n' ||
-		s[i] == '\v' || s[i] == '\f' || s[i] == '\r')
+	while (s[c])
 	{
-		i++;
-	}
-
-	while (s[i] == '-' || s[i] == '+')
-	{
-		if (s[i] == '-')
+		if (s[c] == 45)
 		{
-			sign = -sign;
+			min *= -1;
 		}
-		i++;
+
+		while (s[c] >= 48 && s[c] <= 57)
+		{
+			isi = 1;
+			ni = (ni * 10) + (s[c] - '0');
+			c++;
+		}
+
+		if (isi == 1)
+		{
+			break;
+		}
+
+		c++;
 	}
 
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		result = result * 10 + (s[i] - '0');
-		i++;
-	}
-
-	return (result * sign);
+	ni *= min;
+	return (ni);
 }
