d88a08e8dc8a12030211fc16e7f968550f10fdd1
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-07-04 22:14:29 +0100

    Update 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string to be appended.
+ *
+ * Return: Pointer to the resulting string `dest`.
+ */
+char *_strncat(char *dest, const char *src, int n)
+{
+	int dest_len = 0;
+	int i;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+
+	for (i = 0; src[i] != '\0'; && i < n; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+
+	return (dest);
+}
