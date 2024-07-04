96c0b4c0a7643a4b0dc06f56bedd758c1adc79c4
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-07-05 00:17:48 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,11 +12,16 @@ char *leet(char *str)
 	char *ptr = str;
 	char leet_map[256] = {0};
 
-	leet_map['a'] = '4'; leet_map['A'] = '4';
-	leet_map['e'] = '3'; leet_map['E'] = '3';
-	leet_map['o'] = '0'; leet_map['O'] = '0';
-	leet_map['t'] = '7'; leet_map['T'] = '7';
-	leet_map['l'] = '1'; leet_map['L'] = '1';
+	leet_map['a'] = '4';
+	leet_map['A'] = '4';
+	leet_map['e'] = '3';
+	leet_map['E'] = '3';
+	leet_map['o'] = '0';
+	leet_map['O'] = '0';
+	leet_map['t'] = '7';
+	leet_map['T'] = '7';
+	leet_map['l'] = '1';
+	leet_map['L'] = '1';
 
 	while (*ptr != '\0')
 		{
