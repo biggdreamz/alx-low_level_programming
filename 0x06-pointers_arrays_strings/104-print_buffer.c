739ed587167cb57e6b1c427dc81718e2232876c2
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-07-05 00:59:58 +0100

    Update 104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,33 +1,56 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * print_buffer - prints a s bytes of a buffer
- * @c: buffer to print
- * @s: bytes of buffer to print
- * @l: line of buffer to print
+ * print_buffer - Prints the content of a buffer in a specified format.
+ * @b: Pointer to the buffer to be printed.
+ * @size: Number of bytes to print from the buffer.
  *
- * Return: void
+ * Each line starts with the position of the first byte of the line in hexadecimal (8 chars), starting with 0.
+ * Each line shows the hexadecimal content (2 chars) of the buffer, 2 bytes at a time, separated by a space.
+ * Each line shows the content of the buffer. If the byte is a printable character, print the letter, if not, print '.'.
+ * Each line ends with a new line '\n'.
+ * If size is 0 or less, the output should be a new line '\n'.
  */
-
 void print_buffer(char *b, int size)
 {
-	int j, k;
-
-	for (j = 0; j <= 9; j++)
+	if (size <= 0)
 	{
-		if (j <= s)
-			printf("%02x", c[l * 10 + j]);
-		else
-			printf("  ");
-		if (j % 2)
-			putchar(' ');
+		printf("\n");
+		return;
 	}
-	for (k = 0; k <= s; k++)
+
+	int i, j;
+	unsigned char c;
+
+	for (i = 0; i < size; i += 10)
 	{
-		if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
-			putchar(c[l * 10 + k]);
-		else
-			putchar('.');
+		printf("%08x: ", i);
+
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+			{
+				c = b[i + j];
+				printf("%02x", c);
+				if (j % 2 != 0)
+					printf(" ");
+				else
+					printf(" ");
+			} else
+			{
+				printf("  ");
+			}
+		}
+
+		for (j = 0; j < 10 && i + j < size; j++)
+		{
+			c = b[i + j];
+			if (c >= 32 && c <= 126)
+				printf("%c", c);
+			else
+				printf(".");
+		}
+		printf("\n");
 	}
 }
