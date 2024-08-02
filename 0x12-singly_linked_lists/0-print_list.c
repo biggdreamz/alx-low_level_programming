08adc2db21a3c72eeebc66b216a6ee6b7499634a
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-08-02 11:22:08 +0100

    singly-linked files created

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+*print_list - prints all the elements of a list
+*@h: pointer to the list
+*Return: the number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	const list_t *index = h;
+	size_t count = 0;
+	while (index != NULL)
+	{
+		if (index->str != NULL)
+		{
+			printf("[%d] %s\n", index->len, index->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+count += 1;
+index = index->next;
+	}
+return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+arrays with extra steps done by biggdreams
