5e350208e77295c71081d7985df8c20d678c6ffa
ABUBAKAR ABDUL <abdulabubakarsadiq94@gmail.com>
2024-08-02 23:50:00 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,12 @@
+/**
+ * struct list_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+
 #ifndef LISTS_H
 #define LISTS_H
 
